5c3604c3a4e7341bb10e3637433d89f2cedb6b21
phantom737 <96076197+phantomvoyager4@users.noreply.github.com>
2025-11-30 21:22:51 +0100

    feat: layer and activation relu classes implemented

--- HDR.py
+++ HDR.py
@@ -6,7 +6,8 @@ def data_handling(pathimages, pathlabels):
     images = images / 255.0
     labels = idx2numpy.convert_from_file(pathlabels)
     map = np.identity(10)
-    labels_encoded = map[labels] #one-hot encoding
+    labels_encoded = map[labels]
+    print(images.shape, labels_encoded.shape) #(60000, 784) (60000, 10)
     return images, labels_encoded
 
 
@@ -15,3 +16,18 @@ pathimagess = 'dataset/train-images.idx3-ubyte'
 pathlabelss = 'dataset/train-labels.idx1-ubyte'
 data_handling(pathimagess, pathlabelss)
 
+#NEURONS LOGIC
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons) * 0.01
+        self.biases = np.zeros((1, n_neurons))
+    
+    def fpropagation(self, input):
+        self.output = np.dot(input, self.weights) + self.biases  
+        return self.output
+
+#ACTIVATE NEURONS USING RELU
+class Activation:
+    def forward(self, input):
+        self.output= np.maximum(0, input)
+        return self.output
